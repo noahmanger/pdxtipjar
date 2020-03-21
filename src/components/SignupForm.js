ed46e4ac815a53cf8d726e9d8df82147fd5dfe19
Noah Manger <noah.manger@zapier.com>
2020-03-21 14:02:55 -0700

    Initial commit

---
+++ src/components/SignupForm.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const SignupForm = () => (
+  <iframe
+    title="donate"
+    width="100%"
+    height="800"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSfAW0EtO3hM3xZm_34Z6zFAD1nSGQTTseZUcxw2Hl706yubRA/viewform"
+  ></iframe>
+)
+
+export default SignupForm
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import SignupForm from "../components/SignupForm"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Sign up</h1>
+  </Layout>
+)
+
+export default IndexPage
