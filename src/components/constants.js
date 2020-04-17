8481c75f9b0f6dd759db44a2049c5f3505f3d739
Vicky Cassidy <vicky.volvovski@zapier.com>
2020-04-16 23:50:26 -0500

    Update constants.js

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
 export const SHEETS_URL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTff3X9VJDzdBfD0l6FLflzQ__mBFNrkNs0YWZ0HBpW1wJjoFn_iAb0479OV-VkZDkz7SRA15Yo__dZ/pub?gid=1067431901&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTff3X9VJDzdBfD0l6FLflzQ__mBFNrkNs0YWZ0HBpW1wJjoFn_iAb0479OV-VkZDkz7SRA15Yo__dZ/pub?gid=255304341&single=true&output=csv";
